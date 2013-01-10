c41e69f85e7674add2c419e9822ebe0e49c7e0c8
cliny <cliny@8b8594b5-f669-4727-93f2-ab87e90cbb28>
2013-01-10 20:10:08 +0000

    Set up join and login code. Make a fixed background image.

--- startist/startist.py
+++ startist/startist.py
@@ -2,27 +2,31 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from flask.ext.pymongo import PyMongo
 from pymongo import ASCENDING
 from validate_email import validate_email
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
+import re
 
-DEBUG = False
+DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = "H\xb8\x8do\x8a\xfc\x80\x18\x06\xaf!i\x028\x1bPs\x85\xe7\x87\x11\xe6j\xb1"
 mongo = PyMongo(app)
 
-
+# render the signup page for now until we launch
 @app.route('/')
 def signup():
     return render_template('signup.html')
 
+# validate email submitted from signup page:
+# if the email is invalid, then return 0
+# if the email is valid, then return 1 
+# if the email is valid but already in mongo, then return 2
 @app.route('/email', methods = ['POST'])
 def email():
     email = request.json['email']
     is_valid = validate_email(email,verify=True)
-
     signal = 0
-
     if is_valid:
         mongo.db.emails.ensure_index([("email",ASCENDING)], unique=True, background=True)
         already = mongo.db.emails.find_one(request.json)
@@ -33,10 +37,100 @@ def email():
             mongo.db.emails.insert(request.json)
     else:
         signal = 0
-
     data = {"valid": signal}
     data = json.dumps(data)
     return data
+
+# lander page
+@app.route('/lander')
+def home():
+    return render_template('index.html')
+
+# login user
+@app.route('/login', methods = ['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+    name = ''
+    user = mongo.db.users.find_one({"email": email})
+    if user:
+        email_signal = 1
+        salted_password = user["password"]
+        if check_password_hash(salted_password, password):
+            password_signal = 1
+            session["logged_in"] = True
+            session["name"] = user["name"]
+            name = user["name"]
+            session["user_id"] = str(user["_id"])
+            logins = user["logins"] + 1
+            mongo.db.users.update({"email": email}, {"$set": {"logins": logins}})
+        else:
+            password_signal = 0
+    else:
+        email_signal = 0
+        password_signal = 2
+
+    data = {"name": name, "email": email_signal, "password": password_signal}
+    data = json.dumps(data)
+    return data
+
+# logout user
+@app.route('/logout')
+def logout():
+    session.pop("logged_in", None)
+    session.pop("name", None)
+    session.pop("user_id", None)
+    return redirect(url_for('home'))
+
+# join page
+@app.route('/join', methods = ['POST'])
+def join():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+    confirm = request.json['confirm']
+    is_email_valid = validate_email(email,verify=True)
+
+    if len(name) > 0:
+        name_signal = 1
+    else:
+        name_signal = 0
+
+    if is_email_valid:
+        email_signal = 1
+    else:
+        email_signal = 0
+
+    password_signal = validate_password(password, confirm)
+
+    if name_signal and email_signal and (password_signal == 1):
+
+        mongo.db.users.ensure_index([("email",ASCENDING)], unique=True, background=True)
+        already = mongo.db.users.find_one({"email": email})
+        if already:
+            email_signal = 2
+        else:
+            salted_password = generate_password_hash(password)
+            user = {"name": name, "email": email, "password": salted_password, "logins": 1}
+            user_id = mongo.db.users.insert(user)
+            session["logged_in"] = True
+            session["name"] = name
+            session["user_id"] = str(user_id)        
+
+    data = {"name": name_signal, "email": email_signal, "password": password_signal}
+    data = json.dumps(data)
+    return data
+
+
+# auxillary function
+# validate password
+def validate_password(password, confirm):
+    if password != confirm:
+        return 0
+    if re.match(r'[A-Za-z0-9@#$%^&+=]{7,}', password):
+        return 1
+    else:
+        return 2
 
 
 if __name__ == "__main__":

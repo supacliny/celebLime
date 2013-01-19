709ebe1b24af45018ffa209b94f64f89fd361bad
cliny <cliny@8b8594b5-f669-4727-93f2-ab87e90cbb28>
2013-01-19 02:14:06 +0000

    Get login and signin working like pinterest - username will be the global key.

--- startist/startist.py
+++ startist/startist.py
@@ -3,13 +3,15 @@ from flask.ext.pymongo import PyMongo
 from pymongo import ASCENDING
 from validate_email import validate_email
 from werkzeug.security import generate_password_hash, check_password_hash
+from time import mktime
+from time import time
 import tweepy
 import json
 import re
 import requests
 import urlparse
 
-DEBUG = False
+DEBUG = True
 KEY = 'H\xb8\x8do\x8a\xfc\x80\x18\x06\xaf!i\x028\x1bPs\x85\xe7\x87\x11\xe6j\xb1'
 
 app = Flask(__name__)
@@ -18,21 +20,25 @@ app.config['SECRET_KEY'] = KEY
 mongo = PyMongo(app)
 
 if DEBUG:
-    FB_REDIRECT = 'http://localhost:8000/fb_verify'
+    FB_SIGNUP_REDIRECT = 'http://localhost:8000/fbsverify'
+    FB_LOGIN_REDIRECT = 'http://localhost:8000/fblverify'
     FB_APP_ID = '330274977087606'
     FB_SECRET = 'e9fe610df8d1cbf4ae3dd57226558a29'
     FB_PERMISSIONS = 'user_about_me, email'
     FB_STATE = KEY
-    TW_REDIRECT = 'http://localhost:8000/tw_verify'
+    TW_SIGNUP_REDIRECT = 'http://localhost:8000/twsverify'
+    TW_LOGIN_REDIRECT = 'http://localhost:8000/twlverify'
     TW_CONSUMER_KEY = '9EUuiPXssnwKPwDioQeEyw'
     TW_CONSUMER_SECRET = 'gWXFTi1bDtkbR6Tbu1zRsZ2QHvPkOBh2nsubfA15D8'
 else:
-    FB_REDIRECT = 'http://www.startists.com/fb_verify'
+    FB_SIGNUP_REDIRECT = 'http://www.startists.com/fbsverify'
+    FB_LOGIN_REDIRECT = 'http://www.startists.com/fblverify'
     FB_APP_ID = '471868396209054'
     FB_SECRET = '12d3aa51e0c73758cac9f283fcca23e8'
     FB_PERMISSIONS = 'user_about_me, email'
     FB_STATE = KEY
-    TW_REDIRECT = 'http://www.startists.com/tw_verify'
+    TW_SIGNUP_REDIRECT = 'http://www.startists.com/twsverify'
+    TW_LOGIN_REDIRECT = 'http://www.startists.com/twlverify'
     TW_CONSUMER_KEY = 'QTidL2svjqbtbnDIi3GEg'
     TW_CONSUMER_SECRET = 'GVZ6eApHktuiFJeZRrUQSqc4CwfEfltwi2rC6hTg8'
 
@@ -94,28 +100,22 @@ def home():
 # login user
 @app.route('/signin', methods = ['POST'])
 def signin():
-    email = request.json['email']
+    username = request.json['username']
     password = request.json['password']
-    name = ''
-    user = mongo.db.users.find_one({"email": email})
+    user = mongo.db.users.find_one({"username": username})
     if user:
-        email_signal = 1
+        username_signal = 1
         salted_password = user["password"]
         if check_password_hash(salted_password, password):
             password_signal = 1
-            session["logged_in"] = True
-            session["name"] = user["name"]
-            name = user["name"]
-            session["user_id"] = str(user["_id"])
-            logins = user["logins"] + 1
-            mongo.db.users.update({"email": email}, {"$set": {"logins": logins}})
+            login_user(username)
         else:
             password_signal = 0
     else:
-        email_signal = 0
+        username_signal = 0
         password_signal = 2
 
-    data = {"name": name, "email": email_signal, "password": password_signal}
+    data = {"username": username_signal, "password": password_signal}
     data = json.dumps(data)
     return data
 
@@ -130,9 +130,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop("logged_in", None)
-    session.pop("name", None)
     session.pop("username", None)
-    session.pop("user_id", None)
     return redirect(url_for('home'))
 
 
@@ -144,7 +142,7 @@ def join():
 
 # signup page
 @app.route('/signup')
-def signup_page():
+def email_signup():
     return render_template('signup.html')
 
 
@@ -187,70 +185,161 @@ def register():
 
     if (name_signal == 1) and (username_signal == 1) and (email_signal == 1) and (password_signal == 1):
         salted_password = generate_password_hash(password)
-        user = {"name": name, "username": username, "email": email, "password": salted_password, "logins": 1}
+
+        facebook = session.get("facebook")
+        if facebook == None:
+            facebook = {}
+        else:
+            session.pop("facebook", None)
+
+        twitter = session.get("twitter")
+        if twitter == None:
+            twitter = {}
+        else:
+            session.pop("twitter", None)
+
+        current_time = int(time())
+        ip = request.access_route[0]
+
+        user = {"name": name, "username": username, "email": email, "password": salted_password, "logins": 0, "facebook": facebook, "twitter": twitter, "added_at": current_time, "last_login_at": current_time, "ip": ip}
         user_id = mongo.db.users.insert(user)
-        session["logged_in"] = True
-        session["name"] = name
-        session["username"] = username
-        session["user_id"] = str(user_id)        
+        login_user(username)
 
     data = {"name": name_signal, "username": username_signal, "email": email_signal, "password": password_signal}
     data = json.dumps(data)
     return data
 
 
-@app.route('/facebook')
-def facebook():
+@app.route('/user/<username>', methods = ['GET'])
+def user(username):
+    user = get_user(username)
+    return render_template("profile.html", user=user)
+
+
+# SOCIAL MEDIA LOGIN [
+@app.route('/facebook_login')
+def facebook_login():
     try:
-        fb_app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_REDIRECT, 'scope': FB_PERMISSIONS, 'state': FB_STATE}
-        fb_auth_request = requests.get('https://www.facebook.com/dialog/oauth', params=fb_app_info, allow_redirects=True)
-        fb_oAuthRedirect = fb_auth_request.url
+        fb_app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_LOGIN_REDIRECT, 'scope': FB_PERMISSIONS, 'state': FB_STATE}
+        fb_oAuthRedirect = facebook_connect(fb_app_info)
         return redirect(fb_oAuthRedirect)
-    except Exception:
-        return redirect(url_for('join'))
+    except Exception, e:
+        print e
+        return redirect(url_for('login'))
 
 
-@app.route('/fb_verify')
-def fb_verify():
+@app.route('/fblverify')
+def fblverify():
     try:
         code = request.args['code']
-        app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_REDIRECT, 'client_secret': FB_SECRET, 'code': code}
-        token_request = requests.get('https://graph.facebook.com/oauth/access_token', params=app_info)
-        response = token_request.text
-        access_token = urlparse.parse_qs(response)['access_token'][0]
-        expires = urlparse.parse_qs(response)['expires'][0]
-        graph_params = {'access_token': access_token}
-        user_details = requests.get('https://graph.facebook.com/me/', params=graph_params)
-        user_details = user_details.json()
-        return render_template('signup.html', name=user_details['name'], username=user_details['username'], email=user_details['email'])
-    except Exception:
-        return redirect(url_for('join'))
+        fb_app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_LOGIN_REDIRECT, 'client_secret': FB_SECRET, 'code': code}
+        user_details = facebook_verify(fb_app_info)
+        name = user_details['name']
+        username = user_details['username']
+        email = user_details['email']
+        if check_for_account(username):
+            login_user(username)
+            update_fb_info(user_details)
+            return redirect(url_for('user', username=username))
+        else:
+            session["facebook"] = user_details
+            return render_template('signup.html', name=name, username=username, email=email)
+    except Exception, e:
+        print e
+        return redirect(url_for('login'))
 
 
-@app.route('/twitter')
-def twitter():
+@app.route('/twitter_login')
+def twitter_login():
     try:
-        tw_auth_request = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET, TW_REDIRECT)
-        tw_oAuthRedirect = tw_auth_request.get_authorization_url()
-        session["tw_request_token"] = (tw_auth_request.request_token.key,tw_auth_request.request_token.secret)
+        tw_auth_request = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET, TW_LOGIN_REDIRECT)
+        tw_oAuthRedirect = twitter_connect(tw_auth_request)
         return redirect(tw_oAuthRedirect)
-    except Exception:
-        return redirect(url_for('join'))
+    except Exception, e:
+        print e
+        return redirect(url_for('login'))
 
 
-@app.route('/tw_verify')
-def tw_verify():
+@app.route('/twlverify')
+def twlverify():
     try: 
         verifier = request.args["oauth_verifier"]
-        auth = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET) 
-        token = session.get("tw_request_token")
-        session.pop("tw_request_token", None)
-        auth.set_request_token(token[0], token[1])        
-        auth.get_access_token(verifier)
-        api = tweepy.API(auth)
-        user = api.me()
-        return render_template('signup.html', name=user.name, username=user.screen_name)
-    except Exception:
+        user_details = twitter_verify(verifier)
+        name = user_details['name']
+        username = user_details["screen_name"]
+        if check_for_account(username):
+            login_user(username)
+            update_tw_info(user_details)
+            return redirect(url_for('user', username=username))
+        else:
+            session["twitter"] = user_details        
+            return render_template('signup.html', name=name, username=username)
+    except Exception, e:
+        print e
+        return redirect(url_for('login'))
+# ]
+
+
+# SOCIAL MEDIA SIGNUP [
+@app.route('/facebook_signup')
+def facebook_signup():
+    try:
+        fb_app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_SIGNUP_REDIRECT, 'scope': FB_PERMISSIONS, 'state': FB_STATE}
+        fb_oAuthRedirect = facebook_connect(fb_app_info)
+        return redirect(fb_oAuthRedirect)
+    except Exception, e:
+        print e
+        return redirect(url_for('join'))
+
+
+@app.route('/fbsverify')
+def fbsverify():
+    try:
+        code = request.args['code']
+        fb_app_info = {'client_id': FB_APP_ID, 'redirect_uri': FB_SIGNUP_REDIRECT, 'client_secret': FB_SECRET, 'code': code}
+        user_details = facebook_verify(fb_app_info)
+        name = user_details['name']
+        username = user_details['username']
+        email = user_details['email']
+        if check_for_account(username):
+            login_user(username)
+            update_fb_info(user_details)
+            return redirect(url_for('user', username=username))
+        else:
+            session["facebook"] = user_details
+            return render_template('signup.html', name=name, username=username, email=email)
+    except Exception, e:
+        print e
+        return redirect(url_for('join'))
+
+
+@app.route('/twitter_signup')
+def twitter_signup():
+    try:
+        tw_auth_request = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET, TW_SIGNUP_REDIRECT)
+        tw_oAuthRedirect = twitter_connect(tw_auth_request)
+        return redirect(tw_oAuthRedirect)
+    except Exception, e:
+        print e
+        return redirect(url_for('join'))
+
+
+@app.route('/twsverify')
+def twsverify():
+    try: 
+        verifier = request.args["oauth_verifier"]
+        user_details = twitter_verify(verifier)
+        name = user_details['name']
+        username = user_details['screen_name']
+        if check_for_account(username):
+            login_user(username)
+            update_tw_info(user_details)
+            return redirect(url_for('user', username=username))
+        else:
+            session["twitter"] = user_details        
+            return render_template('signup.html', name=name, username=username)
+    except Exception, e:
+        print e
         return redirect(url_for('join'))
 # ]
 
@@ -262,6 +351,113 @@ def validate_password(password):
         return 1
     else:
         return 0
+
+
+def facebook_connect(fb_app_info):
+    fb_auth_request = requests.get('https://www.facebook.com/dialog/oauth', params=fb_app_info, allow_redirects=True)
+    fb_oAuthRedirect = fb_auth_request.url
+    return fb_oAuthRedirect
+
+
+def facebook_verify(fb_app_info):
+    token_request = requests.get('https://graph.facebook.com/oauth/access_token', params=fb_app_info)
+    response = token_request.text
+    access_token = urlparse.parse_qs(response)['access_token'][0]
+    expires = urlparse.parse_qs(response)['expires'][0]
+    graph_params = {'access_token': access_token}
+    user_details = requests.get('https://graph.facebook.com/me/', params=graph_params)
+    user_details = user_details.json()
+    user_details["access_token"] = access_token
+    user_details["access_token_expires"] = expires    
+    return user_details
+
+
+def twitter_connect(tw_auth_request):
+    tw_oAuthRedirect = tw_auth_request.get_authorization_url()
+    session["tw_request_token"] = (tw_auth_request.request_token.key,tw_auth_request.request_token.secret)
+    return tw_oAuthRedirect
+
+
+def twitter_verify(verifier):
+    auth = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET) 
+    token = session.get("tw_request_token")
+    session.pop("tw_request_token", None)
+    auth.set_request_token(token[0], token[1])        
+    auth.get_access_token(verifier)
+    api = tweepy.API(auth)
+    user = api.me()
+    created_at = int(mktime(user.created_at.timetuple()))
+
+    user_details = { "access_token_key": token[0],
+                     "access_token_secret": token[1],
+                     "contributors_enabled": user.contributors_enabled,
+                     "created_at": created_at,
+                     "description": user.description,
+                     "favourites_count": user.favourites_count,
+                     "followers_count": user.followers_count,
+                     "following": user.following,
+                     "friends_count": user.friends_count,
+                     "geo_enabled": user.geo_enabled,
+                     "twitter_id": user.id,
+                     "lang": user.lang,
+                     "location": user.location,
+                     "name": user.name,
+                     "notifications": user.notifications,
+                     "profile_background_color": user.profile_background_color,
+                     "profile_background_image_url": user.profile_background_image_url,
+                     "profile_background_tile": user.profile_background_tile,
+                     "profile_image_url": user.profile_image_url,
+                     "profile_link_color": user.profile_link_color,
+                     "profile_sidebar_border_color": user.profile_sidebar_border_color,
+                     "profile_sidebar_fill_color": user.profile_sidebar_fill_color,
+                     "profile_text_color": user.profile_text_color,
+                     "profile_use_background_image": user.profile_use_background_image,
+                     "protected": user.protected,
+                     "screen_name": user.screen_name,
+                     "statuses_count": user.statuses_count,
+                     "time_zone": user.time_zone,
+                     "url": user.url,
+                     "utc_offset": user.utc_offset,
+                     "verified": user.verified }
+
+    return user_details
+
+
+def check_for_account(username):
+    user = mongo.db.users.find_one({"username": username})
+    if user:
+        return True
+    else:
+        return False
+
+
+def login_user(username):
+    session["logged_in"] = True
+    session["username"] = username
+    current_time = int(time())
+    ip = request.access_route[0]
+    user = mongo.db.users.find_one({"username": username})
+    logins = user["logins"] + 1
+    mongo.db.users.update({"username": username}, {"$set": {"logins": logins, "last_login_at": current_time, "ip": ip}})
+
+
+def get_user(username):
+    user = mongo.db.users.find_one({"username": username})
+    if user:
+        return user
+    else:
+        return {}
+
+
+def update_fb_info(user_details):
+    username = user_details['username']
+    mongo.db.users.update({"username": username},{"$set": {"facebook": user_details}})
+
+
+def update_tw_info(user_details):
+    username = user_details['screen_name']
+    mongo.db.users.update({"username": username},{"$set": {"twitter": user_details}})
+
 # ]
 
 if __name__ == "__main__":
